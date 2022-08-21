2808a197b4124e3ba41360ea7ba45057ff06e67a
Christian Lhie Besmanos <cell.besmanos0119@gmail.com>
2022-08-21 09:42:52 +0800

    Feat: Style Error route

--- src/routes/error/Error.js
+++ src/routes/error/Error.js
@@ -1,7 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import "./Error.css";
 
 export default function Error() {
-  return <div>Error</div>;
+  return (
+    <main className="Error grid-container">
+      <div className="Error__content flow">
+        <h1 className="ff-serif uppercase">
+          <span
+            className="fs-900 text-white  ff-sans-condensed letter-spacing-2"
+            style={{ display: "block" }}
+            aria-hidden="true"
+          >
+            404
+          </span>
+          Page not found.
+        </h1>
+        <Link to="/" className="Error__cta">
+          Back to home
+        </Link>
+      </div>
+    </main>
+  );
 }
